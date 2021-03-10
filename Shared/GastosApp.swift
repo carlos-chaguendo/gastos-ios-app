db4d4ccdf7e86c5623d2691db1cbd035fb0c3b28
carlos andres chaguendo <c.chaguendo@mayorgafirm.com>
2021-03-10 16:51:23 -0500

    Initial Commit

---
+++ Shared/GastosApp.swift
@@ -0,0 +1,17 @@
+//
+//  GastosApp.swift
+//  Shared
+//
+//  Created by Carlos Andres Chaguendo Sanchez on 10/03/21.
+//
+
+import SwiftUI
+
+@main
+struct GastosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
