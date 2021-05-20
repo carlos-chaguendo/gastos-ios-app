bcc7fc0a2f18b23a02fe1077c5bcc5b304336cb8
carlos andres chaguendo <c.chaguendo@mayorgafirm.com>
2021-05-20 11:19:58 -0500

    Add readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# Gastos
+
+Una aplicacion iOS que permite llevar el control de gastos diarios, escrita con SwiftUI
+
+- Copia de seguridad en iCloud
+- Recordatorios
+- Reportes
+
+
+![alt text](https://raw.githubusercontent.com/carlos-chaguendo/gastos-ios-app/imp.jpg)
+
+
+# Requirements
+
+- Xcode 12.4
+- Swift 5.2
+
+# License
+
+[Carlos Andres Chaguendo Sanchez](https://adivantus.com/)
+[Carlos Andres Chaguendo Sanchez](https://www.google.com/search?q=Carlos+Aandres+Chaguendo)
+[Carlos Andres Chaguendo Sanchez](http://www.sena.edu.co)
+
--- iOS/Transactions/TransactionsByGroupView.swift
+++ iOS/Transactions/TransactionsByGroupView.swift
@@ -8,57 +8,51 @@
 import SwiftUI
 
 struct TransactionsByGroupView<Group: Entity & ExpensePropertyWithValue>: View {
-
+    
     let group: Group
     let groupBy: KeyPath<ExpenseItem, Group>
     let componet: Calendar.Component
     let date: Date
-
+    
     @State private var transactions: [Date: [ExpenseItem]] = [:]
     @State private var dates: [Date] = []
     @State private var points: [CGPoint] = []
-
+    
     @State private var numberOfTransactions = 0
     @State private var total: Double = 0.0
     @State private var max = 0.0
     @State private var min = 0.0
-
+    
+    @Environment(\.colorScheme) var colorScheme
+    
     init(by: KeyPath<ExpenseItem, Group>, for group: Group, in component: Calendar.Component, of date: Date) {
         self.groupBy = by
         self.group = group
         self.componet = component
         self.date = date
     }
-
+    
+    let df = DateFormatter()
+        .set(\.dateFormat, "EEE\ndd")
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
-                
-                
-//                VStack {
-//                Text(DateFormatter.longMonth.string(from: date))
-//                    .fontWeight(.semibold)
-//                    .foregroundColor(Colors.title)
-//                    
-//                    Text(DateFormatter.year.string(from: date))
-//                        .fontWeight(.semibold)
-//                        .foregroundColor(Colors.subtitle)
-//                }.padding(.horizontal)
 
                 VStack {
                     VStack(alignment: .leading) {
-                            Text("Total spending")
-                                .font(.caption)
-                                .foregroundColor(Colors.subtitle)
-                            
-                            Text(NumberFormatter.currency.string(from: NSNumber(value: total)) ?? "")
-                                .font(.title3)
-                                .fontWeight(.medium)
-                                .foregroundColor(Colors.title)
-                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-
-                        }
-
+                        Text("Total spending")
+                            .font(.caption)
+                            .foregroundColor(Colors.subtitle)
+                        
+                        Text(NumberFormatter.currency.string(from: NSNumber(value: total)) ?? "")
+                            .font(.title3)
+                            .fontWeight(.medium)
+                            .foregroundColor(Colors.title)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        
+                    }
+                    
                     Chart.Lines(datasource: [
                         Chart.DataSet(points: points, color: Color(UIColor.from(hex: UInt32(group.color))))
                     ]).frame(height: 120)
@@ -66,52 +60,87 @@ struct TransactionsByGroupView<Group: Entity & ExpensePropertyWithValue>: View {
                 .cardView()
                 .padding()
 
-                ForEach(dates, id: \.self) { date in
+              
 
-                    Text(DateFormatter.day.string(from: date))
-                        .font(.caption)
-                        .foregroundColor(Colors.subtitle)
-                        .padding(.vertical)
+                
+                VStack(spacing: 0) {
+                    ForEach(dates, id: \.self) { date in
+                        
+                        /// Cada dias
+                        HStack(alignment: .top) {
+                            Text(df.string(from: date).capitalized)
+                                .lineLimit(2)
+                                .font(.caption)
+                                .foregroundColor(Colors.subtitle)
+                                .padding(.top)
+                                .frame(width: 60)
+                                .frame(minHeight: 40)
 
-                    ForEach(transactions[date, default: []], id: \.self) { transaction in
-                        PresentLinkView(destination: ExpenseItemFormView(transaction)) {
-                            ExpenseItemView(model: transaction, displayCategory: true)
-                                .padding(.vertical, 4)
-                        }
+                            /// Transacciones de cada dia
+                            VStack {
+                                ForEach(transactions[date, default: []], id: \.self) { transaction in
+                                    PresentLinkView(destination: ExpenseItemFormView(transaction)) {
+                                        ExpenseItemView(model: transaction, displayCategory: true)
+                                            .padding(.vertical)
+                                            .frame(minHeight: 40)
+                                    }
+                                }
+                            }
+                            .padding(.horizontal)
+                            .background(Colors.background)
+                            
+                        }.padding(.leading)
                     }
-                }.padding(.horizontal)
 
-            }.onAppear {
-
+                }
+                //.background(Color.black.opacity(0.2))
+  
+            }
+            .onAppear {
+                
                 let items = Service.transactions(by: groupBy, group, in: componet, of: date)
                 let values = items.map { $0.value }
-
+                
                 self.max = values.max() ?? 0
                 self.min = values.min() ?? 0
                 self.total = values.reduce(0, +)
                 self.numberOfTransactions = items.count
-
+                
                 self.transactions = items.groupBy { $0.date.withStart(of: .day) }
                 self.dates = Array(transactions.keys).sorted()
-
+                
                 let sumByDate = transactions.mapValues { $0.map {$0.value}.reduce(0, +)}
                 let range = Calendar.gregorian.dateInterval(of: componet, for: date)!
                 let dates = range.enumerate(.day)
-
+                
                 var points: [CGPoint] = []
                 var x = 0.0
-
+                
                 for day in dates {
                     let y = sumByDate[day, default: 0.0]
                     points.append(CGPoint(x: x, y: y))
                     x += 1
                 }
-
+                
                 self.points = points
             }
         }
-        .padding(.vertical)
-        .background(Colors.background)
+        .background(
+                ZStack(alignment: .leading) {
+                    Color(Colors.background)
+                    
+                    if self.colorScheme == .dark {
+                        Color.black.opacity(0.3)
+                            .frame(width: 60 + 20)
+                    } else {
+                        Color(Colors.primary)
+                            .opacity(0.1)
+                            .frame(width: 60 + 20)
+                    }
+                    
+                    
+                }.background(Color.red)
+        )
         .navigationTitle(group.name)
     }
 }
@@ -119,6 +148,7 @@ struct TransactionsByGroupView<Group: Entity & ExpensePropertyWithValue>: View {
 struct TransactionsByCategoryView_Previews: PreviewProvider {
     static var previews: some View {
         TransactionsByGroupView(by: \.category, for: Catagory {$0.name = "1"}, in: .month, of: Date())
-
+            //.preferredColorScheme(.dark)
+        
     }
 }
